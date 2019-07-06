074390937b3fe24c0be766d7bdc3be25081e3b51
Kevin Ellerton <kevinellerton@gmail.com>
2019-07-06 12:57:57 -0400

    Updates

---
+++ precache-manifest.bf4d4ae0a0df5e7d8fcadf0d9478ccd4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "935418e84c18f2f20b2bb46256197a88",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fac7339db72599031303",
+    "url": "/monsters-rolodex/static/css/main.9eac9e82.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "fac7339db72599031303",
+    "url": "/monsters-rolodex/static/js/main.9464975f.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
